60fc88d111e03a583db2e0848b89de943e78d9f4
TSanika07 <121048647+TSanika07@users.noreply.github.com>
2024-10-11 10:46:07 +0530

    Create demo.java

---
+++ demo.java
@@ -0,0 +1,8 @@
+class demo{
+public static void main(String args[])
+{
+System.out.println("This java program is generated on Github Platform");
+System.out.println("This java program is executed in Google engine");
+
+}
+}

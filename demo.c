94483fe1bbc40522cc542359cc10aae7573f9815
TSanika07 <121048647+TSanika07@users.noreply.github.com>
2024-10-11 10:25:12 +0530

    Create demo.c

---
+++ demo.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+printf("\n\nThis program is generated on Github Platform \n\n");
+ 
+printf("\n\nThis C program is executed using Google app engine \n\n ");
+return 0;
+}
